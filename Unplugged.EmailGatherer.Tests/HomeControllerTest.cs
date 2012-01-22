e5106ed86d10a05c7dc3e722bd1645d2a91306fc
jfoshee <jacobf@gmail.com>
2012-01-21 20:41:38 -0600

    Added test project and all the solution-level stuff

---
+++ Unplugged.EmailGatherer.Tests/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unplugged.EmailGatherer.Controllers;
+
+namespace Unplugged.EmailGatherer.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void ShouldHaveHomeController()
+        {
+            Assert.IsInstanceOfType(new HomeController(), typeof(Controller));
+        }
+    }
+}
